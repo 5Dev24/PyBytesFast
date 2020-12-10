7358be08f47e02e789ae1344373530ffe4676cb4
Jake H <omega524dev@gmail.com>
2020-12-10 00:27:30 -0600

    Started work on bytecode optimizations
    This should be moved to another repository

--- src/testing/bytecodes.py
+++ src/testing/bytecodes.py
@@ -1,27 +1,312 @@
 from dis import *
 from types import CodeType
 
-testFunc = """
+ArgumentsNeeded = {
+	10: 1, 11: 1, 15: 1, 19: 2,
+	20: 2, 22: 2, 23: 2, 24: 2,
+	25: 2, 26: 2, 27: 2, 28: 2,
+	29: 2, 55: 2, 56: 2, 57: 2,
+	59: 2, 62: 2, 63: 2, 67: 2,
+	75: 2, 76: 2
+}
+
+class Instruct:
+
+	INDEX = 0
+
+	@staticmethod
+	def Reset():
+		Instruct.INDEX = 0
+
+	def __init__(self, optcode: int, arg: int = -1, /, give_id: bool = True):
+		self.optcode = optcode
+		self.arg = arg
+		if give_id:
+			self.id = Instruct.INDEX
+			Instruct.INDEX += 1
+		else:
+			self.id = -1
+
+	def __str__(self) -> str:
+		return f"{self.id:->3} {self.optcode:<3} {self.arg:>3}"
+
+	def __repr__(self) -> str:
+		return f"{self.id} {self.optcode} {self.arg}"
+
+class Function:
+
+	def __init__(self, instructs: list, init_code: CodeType = None):
+		self.instructs = instructs
+		self.init_code = init_code
+		self.returns = self.single_use_vars = self.constant_opts = self.dead_vars = None
+		self.__cycle()
+
+	def __cycle(self):
+		print("Pre\n", "=" * 50, "\n", self, "\n", "=" * 50, sep="")
+		ret = False
+		passCount = 0
+		while not ret:
+			passCount += 1
+			print("Pass", passCount)
+			self.__map()
+			ret = self.__optimize()
+			print("=" * 50, "\n", self, "\n", "=" * 50, sep="")
+
+		#TODO: return modified init_code
+
+	def __map(self):
+		returns = []
+		single_use_vars = []
+		constant_opts = []
+		dead_vars = []
+
+		for instruct in self.instructs:
+			if instruct.optcode == 83:
+				returns.append(instruct)
+
+			if (instruct.optcode >= 10 and instruct.optcode <= 11) or\
+				(instruct.optcode >= 19 and instruct.optcode <= 29) or\
+				(instruct.optcode >= 55 and instruct.optcode <= 57) or\
+				(instruct.optcode >= 62 and instruct.optcode <= 63) or\
+				(instruct.optcode >= 75 and instruct.optcode <= 76) or\
+				instruct.optcode in (59, 67): #TODO: Add support for COMPARE_OP to allow for boolean resolving and then JUMP support
+
+				if self.__are_args_const(instruct, self.__prior(instruct, 2)):
+					constant_opts.append(instruct)
+
+		if self.init_code is not None:
+			for loc in range(self.init_code.co_nlocals):
+				assigns, uses = len(self.__assignments(loc)), len(self.__uses(loc))
+				if assigns == 1 and uses == 1:
+					single_use_vars.append(loc)
+
+			for con in range(len(self.init_code.co_consts)):
+				uses =len(self.__uses_const(con))
+				if assigns == 0 or uses == 0:
+					dead_vars.append(con)
+
+		self.returns = tuple(returns)
+		self.single_use_vars = tuple(single_use_vars)
+		self.constant_opts = tuple(constant_opts)
+		self.dead_vars = tuple(dead_vars)
+
+	def __optimize(self):
+		print("Dead:", self.dead_vars)
+
+		#TODO: Remove dead
+
+		print("Single:", self.single_use_vars)
+
+		singles_to_remove = {}
+		for var in self.single_use_vars:
+			store = self.__assignments(var)[0]
+			if store.id > 0 and (load_struct := self.__find(store.id - 1, self.__prior(store))) is not None:
+				if load_struct.optcode == 100:
+					singles_to_remove[var] = [store, self.__uses(var)[0], load_struct]
+					print(f"The single use of {var} could be replaced with a LOAD_CONST of {load_struct.arg}")
+
+		if len(singles_to_remove):
+
+			for var, values in singles_to_remove.items():
+				#self.__replaceInstruct(values[2].id, Instruct(100, values[2].arg, give_id = False))
+				self.__removeInstruct(values[1].id)
+				self.__removeInstruct(values[0].id)
+				#TODO: remove from co_varnames
+
+			return False # Map and Optimize again
+
+		if self.init_code is not None:
+			print("Const Opts:", self.constant_opts)
+
+			ops_resolved = {}
+			for instruct in self.constant_opts:
+				args = [self.init_code.co_consts[i.arg] for i in self.__prior(instruct, 2)[:ArgumentsNeeded[instruct.optcode]] if i.optcode == 100]
+				optcode = instruct.optcode
+				value = None
+
+				#TODO: Add INPLACE modification of consts instead of using the return then let the dead vars find the one if it died
+
+				if optcode == 10: # UNARY_POSITIVE
+					value = +args[0]
+
+				elif optcode == 11: # UNARY_NEGATIVE
+					value = -args[0]
+
+				elif optcode == 19: # BINARY_POWER
+					value = args[1] ** args[0]
+
+				elif optcode == 20: # BINARY MULTIPLY
+					value = args[1] * args[0]
+
+				elif optcode == 22: # BINARY_MODULO
+					value = args[1] % args[0]
+
+				elif optcode == 23: # BINARY_ADD
+					value = args[1] + args[0]
+
+				elif optcode == 24: # BINARY_SUBTRACT
+					value = args[1] - args[0]
+
+				elif optcode == 25: # BINARY_SUBSCR
+					value = args[1][args[0]]
+
+				elif optcode == 26: # BINARY_FLOOR_DIVIDE
+					value = args[1] // args[0]
+
+				elif optcode == 27: # BINARY_TRUE_DIVIDE
+					value = args[1] / args[0]
+
+				elif optcode == 28: # INPLACE_FLOOR_DIVIDE
+					args[1] //= args[0]
+
+				elif optcode == 29: # INPLACE_TRUE_DIVIDE
+					args[1] /= args[0]
+
+				elif optcode == 55: # INPLACE_ADD
+					args[1] += args[0]
+
+				elif optcode == 56: # INPLACE_SUBTRACT
+					args[1] -= args[0]
+
+				elif optcode == 57: # INPLACE_MULTIPLY
+					args[1] *= args[0]
+
+				elif optcode == 59: # INPLACE_MODULO
+					args[1] %= args[0]
+
+				elif optcode == 62: # BINARY_LSHIFT
+					value = args[1] << args[0]
+
+				elif optcode == 63: # BINARY_RSHIFT
+					value = args[1] >> args[0]
+
+				elif optcode == 67: # INPLACE_POWER
+					args[1] **= args[0]
+
+				elif optcode == 75: # INPLACE_LSHIFT
+					args[1] <<= args[0]
+
+				elif optcode == 76: # INPLACE_RSHIFT
+					args[1] >>= args[0]
+
+				#TODO: Modify constants, add value and args[1] as args[0] is unchanging
+
+		print("Returns:", self.returns)
+		for ret in self.returns:
+			prior: list = self.__prior(ret)
+			assigns: list = self.__assignments(ret.arg, prior)
+
+		return True # Terminate
+
+	def __are_args_const(self, operation: Instruct, instructs: list = None):
+		const_needed = ArgumentsNeeded[operation.optcode]
+		const_gotten = sum([1 for instruct in instructs[:const_needed] if instruct.optcode == 100])
+		return const_gotten >= const_needed
+
+	def __removeInstruct(self, id: int):
+		for i in range(len(self.instructs)):
+			instruct = self.instructs[i]
+			if instruct is None: continue
+
+			if instruct.id == id: self.instructs[i] = None
+			elif instruct.id > -1 and instruct.id > id: instruct.id -= 1
+
+		i = len(self.instructs) - 1
+		while i > 0:
+			if self.instructs[i] is None:
+				del self.instructs[i]
+
+			i -= 1
+
+	def __replaceInstruct(self, id: int, replacement: Instruct):
+		for i in range(len(self.instructs)):
+			instruct = self.instructs[i]
+			if instruct is None: continue
+
+			if instruct.id == id:
+				replacement.id = id
+				self.instructs[i] = replacement
+
+	def __insertInstruct(self, id: int, insert: Instruct):
+		for i in range(len(self.instructs)):
+			instruct = self.instructs[i]
+			if instruct.id > id:
+				instruct.id += 1
+			elif instruct.id == id:
+				insert.id = id
+				instruct.id += 1
+				self.instructs.insert(i, insert)
+
+	def __find(self, id: int, instructs: list = None):
+		if instructs is None: instructs = self.instructs
+
+		for instruct in instructs:
+			if instruct.id == id:
+				return instruct
+
+	def __uses_const(self, index: int, instructs: list = None):
+		if instructs is None: instructs = self.instructs
+
+		return [instruct for instruct in instructs if instruct is not None and instruct.arg == index and instruct.optcode in (100,)]
+
+	def __assignments(self, index: int, instructs: list = None):
+		if instructs is None: instructs = self.instructs
+
+		return [instruct for instruct in instructs if instruct is not None and instruct.arg == index and instruct.optcode in (125,)]
+
+	def __uses(self, index: int, instructs: list = None):
+		if instructs is None: instructs = self.instructs
+
+		return [instruct for instruct in instructs if instruct is not None and instruct.arg == index and instruct.optcode in (124,)]
+
+	def __prior(self, final: Instruct, seek: int = 0, instructs: list = None):
+		if instructs is None: instructs = self.instructs
+		found = 0
+		gotten = []
+
+		for instruct in instructs:
+			if instruct is None: continue
+
+			if (seek < 1 or found < seek) and instruct.id < final.id:
+				gotten.append(instruct)
+
+		return gotten
+
+	def __str__(self):
+		output = ""
+		for instruct in self.instructs:
+			output += f"{instruct.id:<3} {instruct.optcode:>3} {instruct.arg:<3}\n"
+		return output.rstrip("\n")
+
+initialFunction = """
 def test():
-    testA = 29
-    testB = 18
-    testC = testA * testB
-    return testC
+	testA = 29
+	testB = 18
+	testC = testA * testB
+	return testC
 """
 
-testCompile = compile(testFunc, "<string>", "exec")
+initialCompile = compile(initialFunction, "<string>", "exec")
 
-testFuncCompiled = testCompile.co_consts[0]
+initialCompiled = initialCompile.co_consts[0]
 
-print("Code:")
-show_code(testFuncCompiled)
+#print("Code:")
+#show_code(initialCompiled)
 
-print("\nInstructions:")
-for instruction in get_instructions(testFuncCompiled):
-    print("\t", instruction, sep="")
+#print("\nInstructions:")
+#dis(initialCompiled)
 
+codes: bytes = initialCompiled.co_code
+#print(codes)
+
+instructs: list = [Instruct(codes[i], codes[i + 1]) for i in range(0, len(codes), 2)]
+Instruct.Reset()
+
+func: Function = Function(instructs, initialCompiled)
+
+print(func)
 
 finalFunc = """
 def test():
-    return 522
+	return 522
 """
